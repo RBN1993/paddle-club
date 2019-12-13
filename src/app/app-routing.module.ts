d80900a574cd6664f38740b29810c68959d4f9e3
rbn1993 <surfacerobin@outlook.com>
2019-12-13 02:28:23 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: 'news', loadChildren: () => import('./pages/news/news.module').then(m => m.NewsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/news/news.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { NewsRoutingModule } from './news-routing.module';
+
+import { NewsComponent } from './news.component';
+
+
+@NgModule({
+  imports: [NewsRoutingModule],
+  declarations: [NewsComponent],
+  exports: [NewsComponent]
+})
+export class NewsModule { }
